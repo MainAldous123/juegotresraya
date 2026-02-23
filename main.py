df07885c33ab5a83ab8ed7ff261c8e15ba68bdce
Nando <fernandosurcochoa@gmail.com>
2026-02-22 23:03:48 -0500

    Refactor: Cambia título en main para generar conflicto

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 
 # Crear ventana
 ventana = tk.Tk()
-ventana.title("🎮 Juego Tres en Raya Version FEATURE")
+ventana.title("🎮 Juego Tres en Raya VERSION MAIN CONFLICT")
 ventana.geometry("400x450")
 ventana.config(bg="#1e1e2f")
 
